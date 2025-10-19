47b8270edc12ceaf0ee38a651fecc343936e6be1
MOHITW27 <mohit.walecha@gmail.com>
2025-10-19 13:45:11 +0530

    Commit before rebase

--- feature.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a,b):
-            return a + b
-
-result = add_numbers(a=int(input(" Enter 1 st number : ")),b=int(input(" Enter 2 nd number : ")))
-
-print("The sum of two numbers is " ,result)
-
-# editing feature x branch again to check for merge commit
