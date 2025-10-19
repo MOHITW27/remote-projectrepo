19beefa3d69567390c6bb1f7f34ea6284bc716fa
MOHITW27 <mohit.walecha@gmail.com>
2025-10-19 12:05:17 +0530

    Task2 New file feature.py added to featurex branch

---
+++ feature.py
@@ -0,0 +1,6 @@
+def add_numbers(a,b):
+            return a + b
+
+result = add_numbers(a=int(input(" Enter 1 st number : ")),b=int(input(" Enter 2 nd number : ")))
+
+print("The sum of two numbers is " ,result)
